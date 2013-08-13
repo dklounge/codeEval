5e0de243f6c57a3fa2a52fc72a7cd16b1200e48e
dklounge <daviddarden11@gmail.com>
2013-08-12 17:41:40 -0700

    updated intersection

--- 30setIntersection.rb
+++ 30setIntersection.rb
@@ -1,8 +1,8 @@
-require 'set'
-
+# for submission use ARGF.each_line do |line|
 File.open("file").each_line do |line|
-  newset = set(line.split(';'))
-
-  print newset
-
+  set = line.split(';').map do |num|
+    num.split(',').map(&:to_i)
+  end
+  p set.inject(:&)
+  # set.inject(:&).join(',') if set.size == 2
 end
