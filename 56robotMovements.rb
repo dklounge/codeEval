aee98fe27e9d4e756511c2fcdf4e6a008b39171a
dklounge <daviddarden11@gmail.com>
2013-08-19 20:56:32 -0700

    new file

---
+++ 56robotMovements.rb
@@ -0,0 +1,7 @@
+'''
+A robot is located at the top-left corner of a 4x4 grid.
+The robot can move either up, down, left, or right, but can not visit the same spot twice.
+The robot is trying to reach the bottom-right corner of the grid.
+'''
+
+puts 'hello world'
